a8f78bb84c4b8162c6c0a77d9cc55a26399d2369
Elias Kelta <eliaskelta1@gmail.com>
2025-10-12 15:11:53 +0200

    Add l10n for metric label in German

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/elias/flutter/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/elias/OpenNutriTracker" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+41" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 41 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/elias/flutter/flutter"
+  "PROJECT_DIR=/home/elias/OpenNutriTracker"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/elias/OpenNutriTracker/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/elias/OpenNutriTracker/lib/main.dart"
+)
